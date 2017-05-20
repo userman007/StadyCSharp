eca45c89f78382dbc6934c156a9b059b4ece2479
Nikolai <kolisnyak@gmail.com>
2017-05-20 11:31:12 +0300

    Added Log folder

---
+++ OOP/BaseLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Log
+{
+	public class BaseLog
+	{
+		protected List<Person> people;
+		
+		public string GetName(int index)
+		{
+			return string.Format("{0} {1}", people[index].FirstName, people[index].LastName);
+		}
+
+		public void AddPerson(Person person)
+		{
+			if (person == null) return;
+			if (people == null) people = new List<Person>();
+			people.Add(person);
+		}
+
+		public virtual string ToHtml()
+		{
+			StringBuilder sb = new StringBuilder();
+			for (var i = 0; i < people.Count; i++)
+				sb.Append(GetName(i));
+			return sb.ToString();
+		}
+	}
+}
